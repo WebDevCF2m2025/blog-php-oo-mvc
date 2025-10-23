b57c8082b713e7abd88277c1063465e19b3fb14d
mikhawa <michaeljpitz@gmail.com>
2025-10-23 16:16:58 +0200

    admin begin

--- controller/adminController.php
+++ controller/adminController.php
@@ -1,2 +1,23 @@
 <?php
-echo "Admin";
+// path : controller/adminController.php
+
+// chemin vers les dépendances
+use model\manager\CategoryManager;
+use model\manager\ArticleManager;
+use model\manager\UserManager;
+use model\manager\CommentManager;
+use model\mapping\CommentMapping;
+
+// création des managers utiles
+$categoryManager = new CategoryManager($connectPDO);
+$articleManager = new ArticleManager($connectPDO);
+$userManager = new UserManager($connectPDO);
+$commentManager = new CommentManager($connectPDO);
+
+
+echo $twig->render('backend/homepage.back.html.twig', [
+    // racine URL pour les liens
+    'racineURL' => RACINE_URL,
+    // la session pour savoir si l'utilisateur est connecté
+    'session' => $_SESSION ?? [],
+]);
