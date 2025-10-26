8dc083b7c996cbdac7481f7c9afaeff014e8864a
mikhawa <michaeljpitz@gmail.com>
2025-10-26 20:16:48 +0100

    js redirect

--- controller/adminController/categoryController.php
+++ controller/adminController/categoryController.php
@@ -3,3 +3,10 @@
 
 
 echo "Category en cours de création";
+
+?>
+<script>
+    setTimeout(() => {
+        window.location.href = "<?=RACINE_URL?>/admin/";
+    }, 2000);
+</script>
--- controller/adminController/userController.php
+++ controller/adminController/userController.php
@@ -2,3 +2,9 @@
 // path : controller/userController.php
 
 echo "User en cours de création";
+?>
+<script>
+    setTimeout(() => {
+        window.location.href = "<?=RACINE_URL?>/admin/"
+    }, 2000);
+</script>
